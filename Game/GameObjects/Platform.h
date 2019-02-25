999321d873373fea21a1234cc0742ae4fe977be3
Matthew Troscianczuk <35976924+MattTros@users.noreply.github.com>
2019-02-25 12:13:11 -0500

    Update Platform.h

--- Game/GameObjects/Platform.h
+++ Game/GameObjects/Platform.h
@@ -14,7 +14,7 @@ public:
 	~Platform();
 
 	//Platform Specific methods
-	void onCollision(GameObject* other_);
+	void OnCollision(GameObject* other_);
 	void Update(const float deltaTime_);
 
 private:
